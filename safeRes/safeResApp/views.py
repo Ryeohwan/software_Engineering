6045f2445dab97b8b5ed7486d99b940191cb9c8a
orlando <59018852+ingkoon@users.noreply.github.com>
2020-11-20 12:00:32 +0900

    부트스트랩 연동

--- safeRes/safeResApp/views.py
+++ safeRes/safeResApp/views.py
@@ -3,6 +3,6 @@ from django.views import View
 
 # Create your views here.
 
-class safeMaps(request):
+def safeMaps(request):
     #가장 먼저 사용자의 요청에 따라 메인 페이지 호?
-    return render(request, 'safeResApp/safeMaps.html',{})
+    return render(request, 'index.html',{})
